386c8aba191d2a1c75afe10f0f8450c6af64ba00
Aryan Koshti <aryankosti120@gmail.com>
2026-04-18 07:30:56 +0530

    added user-routes and updated app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,6 +15,8 @@ const io = connectToSocket(server);
 
 app.set("port", (process.env.PORT || 8000))
 app.use(cors());
+app.use(express.json({limit: "40kb"}));
+app.use(express.urlencoded({limit:"40kb", extended: true}));
 
 const start = async () => {
     try {
